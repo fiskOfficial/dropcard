9633005e4f88b7d11a5cb7264b8b8e5eb873f298
fiskOfficial <132220682+fiskOfficial@users.noreply.github.com>
2023-06-20 13:55:00 +0300

    theLastBeforeClass

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ createCard.style.display = "flex";
 createCard.style.justifyContent = "center";
 createCard.style.alignItems = "center";
 createCard.style.color = "white";
-createCard.style.border = "white 4px dashed"
+createCard.style.border = "white 4px dashed";
 
 h1.parentNode.insertBefore(createCard, h1.nextSibling);
 
 createCard.addEventListener("click", () => {
     
     const newNote = document.createElement("div");
-    newNote.classList.add(`note`)
+    newNote.classList.add(`note`);
+    
 
     newNote.style.width = 130 + "px";
     newNote.style.height = 50 + "px";
@@ -33,14 +34,20 @@ createCard.addEventListener("click", () => {
      const newColor = `rgb(${R}, ${G}, ${B} )`
     createCard.style.backgroundColor = newColor;
     newNote.style.backgroundColor = newColor;
+    
  
     const randomPositionX = Math.random() * window.innerWidth / 1.5; 
     const randomPositionY = Math.random() * window.innerHeight /1.5; 
     newNote.style.position = "absolute";
     newNote.style.transform = `translate(${randomPositionX}px, ${randomPositionY}px)`
 
+    newNote.setAttribute("draggable", "true");
+
     createCard.parentNode.insertBefore(newNote, createCard.nextSibling);
     console.log(newNote);
+    
+
+    
 
     // newNote.addEventListener("dblclick", () => {
     //     const input = document.createElement("input");
@@ -54,7 +61,9 @@ createCard.addEventListener("click", () => {
     //   });
 
     newNote.addEventListener("dblclick", () => {
+
         const input = document.createElement("input");
+        
         if(newNote.innerHTML !== "") {
             newNote.innerHTML = "";
         }
@@ -62,11 +71,14 @@ createCard.addEventListener("click", () => {
         input.style.backgroundColor = newColor;
         input.style.border = "white 1px solid";
         input.style.width = 90 + "%";
-
-        console.log("Double click event on note");
         input.style.position = "relative"; 
         input.style.transform = "none"; 
+        newNote.style.overflow = "scroll";
+
+
         newNote.appendChild(input); 
+        newNote.style.zIndex = 1000;
+
         if(R+G+B < 305){
             newNote.style.color = "white";
             input.style.color = "white";
@@ -75,23 +87,52 @@ createCard.addEventListener("click", () => {
             input.style.color = "black"
         }
 
+        
+
         input.addEventListener("keydown" , (event) => {
-            console.log(event.code);
             if (event.code === "Enter") {
                 input.remove();
                 newNote.innerHTML = input.value;
+                newNote.style.zIndex = 0;
             }
         })
+       
       });
-    //   newNote.addEventListener("mousedown", () => {
-    //     document.onmousemove(e => {
-    //         var x = e.pageX; 
-    //         var y = e.pageY; 
 
-    //         newNote.style.left = x + "px";
-    //         newNote.style.top = y + "px";
-    //     })
-    //   })
+         
+//       newNote.addEventListener("mousedown", (e) => {
+//         console.log(e);
+//         let x = e.x
+//         let y = e.y
+         
+//          newNote.style.transform = `translate(${x}px, ${y}px)`
+// })
+//         newNote.addEventListener("mouseup", (e) =>{ 
+//         console.log(e);
+
+//         let x = e.pageX;
+//         let y = e.pageY;
+
+        
+//         newNote.style.transform = `translate(${x}px, ${y}px)`
+// })
+
+// newNote.addEventListener("mousedown", (e) => {
+//     const draggingTryout = document.querySelectorAll(".note");
+//     console.log(draggingTryout);
+//     console.log(e);
+// })
+
+// newNote.addEventListener("mouseup", (e) => {
+    
+//     console.log(e)
+// })
 })
+
+
+
+
+
+
 
 
